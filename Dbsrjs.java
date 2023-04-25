5804be84bbd362f2dff268367931b9215222deb6
dbsrjs <124150775+dbsrjs@users.noreply.github.com>
2023-04-25 17:30:16 +0900

    JAVA 방과후(구구단)

---
+++ Dbsrjs.java
@@ -0,0 +1,19 @@
+package dbs;
+
+import java.util.Scanner;
+
+public class Dbsrjs 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=9; i++) 
+		{
+			for(int j =1; j<=9; j++) 
+			{
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
+	}	
+}
