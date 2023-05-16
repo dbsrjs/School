dcba6abf895cb2ceb7fdcf059f3746eb6002d16e
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2023-05-16 17:17:08 +0900

    반복문 문제

---
+++ dbs.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class dbs
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("start : ");
+		int start = sc.nextInt();
+		
+		System.out.print("last : ");
+		int last = sc.nextInt();
+		
+		System.out.println("val : ");
+		int val = sc.nextInt();
+		
+		int sum = 0;
+		
+		for (int i = start; i <= last; i+=val) 
+		{
+			System.out.println("i : " + i);
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
