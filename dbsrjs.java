21195c906c00e8998352537b31f9358ff49bd05a
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2023-05-23 17:17:12 +0900

    2, 3의 배수의 합 출력(JAVA)

--- dbsrjs.java
+++ dbsrjs.java
@@ -5,45 +5,26 @@ public class dbsrjs
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-        // 1~40까지 진행한다. for문
-        for(int i=1; i<=40; i++)
-        {
-        	int 언제까지 = sc.nextInt();
-            int 박수 = 0;
-           
-            while(true) 
-            {
-            	int num = i;
-                System.out.println(num);
-                if(num%10==3 || num%10==6 || num%10==9) 
-                {
-                    System.out.println(num+": 짝");
-                    박수++;
-                }
-                num = num / 10;
-                if(num==0) 
-                {
-                    break;
-                }
-            }
-            
-           System.out.printf("1~4까지의 총 박수 수 : %d", 언제까지, 박수);
-        }
-       
-       
-        // 3, 6, 9가 들어간 숫자는 박수를 친다.
-        // 10으로 나눴을때 나머지가 3,6,9  
-        // 6321
-        // 6321 % 10 = 1
-        // 6321 / 10 = 632
-       
-        //  632 % 10 = 2
-        //  632 / 10 = 63
-       
-        //   63 % 10 = 3------>짝
-        //   63 / 10 = 6
-       
-        //    6 % 10 = 6------>짝
-        //    6 / 10 = 0 <------- 0이 될때까지               
+		
+		System.out.println("값을 입력해주세요");
+		int a = sc.nextInt();
+		
+		int sum_1 = 0;
+		int sum_2 = 0;
+		
+		for (int i = 1; i <= a; i++)
+		{
+			if (i%2 == 0)
+			{
+				sum_1 += i;
+			}
+			
+			else if (i%2 != 0)
+			{
+				sum_2 += i;
+			}
+		}
+		System.out.println("짝숭의 합 : " + sum_1);
+		System.out.println("홀수의 합 : " + sum_2);		
 	}
 }
