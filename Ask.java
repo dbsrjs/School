48fad18ca8da34bffacc086ae4e553053a6aee6b
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2023-07-12 16:42:05 +0900

    아스키코드(JAVA)

---
+++ Ask.java
@@ -0,0 +1,19 @@
+package jv_0712;
+
+public class Ask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       
+        char a = 'S';
+        char b = 'U';
+        char c = 'N';
+        char d = 'G';
+        char e = 'I';
+        char f = 'L';
+       
+        int sum = a+b+c+d+e+f;
+       
+        System.out.println(sum);        
+    }
+}
