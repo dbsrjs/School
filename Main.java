09ab34a5abdc39593b093975cc8fa2fc1db4be01
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2023-07-12 16:43:02 +0900

    5월16일 자바 시험(JAVA)

--- Main.java
+++ Main.java
@@ -1,34 +1,29 @@
-package main;
+package jv_0712;
 
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("체중 입력");
-		float a = sc.nextFloat();
-		System.out.print("키 입력");
-		float b = sc.nextFloat();
-					
-		float bmi = (a/(b*b));
-		
-		if (bmi < 18.5) 			{
-			System.out.println("저체중");			
-		}
-		
-		else if (bmi <= 24.9)		{
-			System.out.println("건강");
-		}
-		
-		else if (bmi <= 29.9) 	{
-			System.out.println("과체중");
-		}
-		
-		else if (bmi >= 30)		{
-			System.out.println("비만");
-		}
+        // 시작 : 1, 마지막 : 99, 증가값 : 2
+        System.out.print("start?: ");
+        int 시작 = sc.nextInt();
+
+        System.out.print("last?: ");
+        int 마지막 = sc.nextInt();
+
+        System.out.print("value change?: ");
+        int 증가값 = sc.nextInt();
+       
+        int sum = 0;
+       
+        for(int i=시작; i<=마지막; i+=증가값) {
+            System.out.println("i: "+i);
+            sum = sum + i;
+        }
+        System.out.println(sum);
 	}
+
 }
