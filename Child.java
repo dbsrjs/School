c1c1f5a2d135126bb940c327b67d22f82da5a783
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-04 17:34:11 +0900

    06/04 Object

--- Child.java
+++ Child.java
@@ -1,7 +1,10 @@
-package poly.basic;
+package lang.object;
 
 public class Child extends Parent{
     public void childMethod() {
         System.out.println("Child.childMethod");
     }
 }
+/*
+클래스에 상속 받을 부모 클래스를 명시적으로 지정하면 object를 상속 받지 않는다.
+ */
