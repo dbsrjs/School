f6513d3148f9bae995d486c00530453436ee0160
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-11 17:20:43 +0900

    06/11 Java Level3 Test

--- Main.java
+++ Main.java
@@ -1,21 +1,29 @@
-package removecontrolflag.after;
+package poly.sort;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        SimpleDataBase simpleDataBase = new SimpleDataBase(new FileReader(".\\src\\datafile.txt"));
+        //객체화 부분 작성1
+        Sort[] sort = {new BubbleSort(), new HeapSort(), new QuickSort()};
 
-        Iterator<String> ir = simpleDataBase.iterator();
+        int[] nums = new int[5];
 
-        while(ir.hasNext()) {
-            String key = ir.next();
-            System.out.println("key:" + key);
-            System.out.println("value: " + simpleDataBase.getValue(key));
-        }
+        System.out.println("정렬 방식을 선택하세요.");
+        System.out.println("[1] : BubbleSort");
+        System.out.println("[2] : HeapSort");
+        System.out.println("[3] : QuickSort");
+
+        int choice = sc.nextInt();
+
+        selectedSort(sort[choice - 1], nums);
+    }
+
+    private static void selectedSort(Sort sort, int[] nums)
+    {
+        sort.ascending(nums);
+        sort.descending(nums);
+        sort.description();
     }
 }
