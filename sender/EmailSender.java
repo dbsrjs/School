bba78ba8cb38f1619fe8ead42f8dc6f560e05a10
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 15:04:17 +0900

    Delete sender directory

--- sender/EmailSender.java
+++
@@ -1,9 +0,0 @@
-package poly.ex.sender;
-
-public class EmailSender implements Sender{
-
-    @Override
-    public void sendMessage(String ms) {
-        System.out.println("메일을 발송합니다: " + ms);
-    }
-}
--- sender/SmsSender.java
+++
@@ -1,9 +0,0 @@
-package poly.ex.sender;
-
-public class SmsSender implements Sender{
-
-    @Override
-    public void sendMessage(String ms) {
-        System.out.println("SMS를 발송합니다: " + ms);
-    }
-}
